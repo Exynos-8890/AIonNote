70e512e7032180fa730aea5906074f3e75ba8fea
Tianlu <zhutl@shanghaitech.edu.cn>
2024-04-23 21:32:40 +0800

    add form and default_values

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from tools.db2mermaid import db2mermaid_code
 from tools.read_db import read
 
 app = Flask(__name__)
+app.debug = True
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db') # 数据库文件保存在根目录
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 # db = SQLAlchemy(app)
@@ -24,6 +25,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html',mermaid_code=db2mermaid_code())
+
+@app.route('/add',methods=['GET', 'POST'])
+def add():
+    # 设置单行文字输入表单的初始内容
+    summary_input_value = "节点缩写"
+
+    # 设置多选框的选项
+    checkbox_options = ["选项1", "选项2", "选项3"]
+    selected_options = []
+
+    prompt_input_value = "请在此输入问题"
+
+    return render_template('adjust.html', 
+                           summary_input_value=summary_input_value, checkbox_options=checkbox_options, selected_options=selected_options, prompt_input_value=prompt_input_value)
+
+
 
 @app.route('/test')
 def given_tasks():
